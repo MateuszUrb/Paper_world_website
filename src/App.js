7f5e4998704ffad43ea5d577ebc4ed4f5ec63fa3
Mateusz Urban <98m.urban@gmail.com>
2021-07-07 23:50:07 +0200

    added animation while site is fetching and while changing routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.scss';
 import mailgo, { MailgoConfig } from 'mailgo';
-import { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import BookSearchForm from './components/BookSearchForm';
-import Footer from './components/Footer';
-import Book from './components/BookDetails';
-import NotFound from './components/404.jsx';
+import Loading from './components/Loading.jsx';
+
+import style from './assets/styles/loadingPage.module.scss';
+import { ReactComponent as ResultSVG } from './assets/images/result_svg.svg';
+
+const Footer = React.lazy(() => import('./components/Footer'));
+const Book = React.lazy(() => import('./components/BookDetails'));
+const NotFound = React.lazy(() => import('./components/404.jsx'));
 
 const mailgoConfig: MailgoConfig = {
   dark: true,
@@ -26,15 +31,26 @@ function App() {
   }, []);
   return (
     <>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/book/:id" component={Book} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
+      <Suspense fallback={<LoadingPage />}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            <Route exact path="/book/:id" component={Book} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Router>
+      </Suspense>
     </>
   );
 }
+
+const LoadingPage = () => (
+  <div className={style.loading__wrapper}>
+    <ResultSVG />
+    <div className={style.loading__wrapper_card}>
+      <Loading />
+    </div>
+  </div>
+);
 
 export default App;
